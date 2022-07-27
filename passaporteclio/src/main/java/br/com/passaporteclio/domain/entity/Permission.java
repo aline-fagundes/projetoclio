c987ce6a9a8129b07ee280f6371a4e3d78397524
Aline Napoli Fagundes <aline.martinsnapoli@hotmail.com>
2022-07-27 19:31:49 -0300

    Correção da mensagem de erro para validação de DTO

--- passaporteclio/src/main/java/br/com/passaporteclio/domain/entity/Permission.java
+++ passaporteclio/src/main/java/br/com/passaporteclio/domain/entity/Permission.java
@@ -14,7 +14,8 @@ public class Permission implements GrantedAuthority, Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	@Id	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String perfil;
