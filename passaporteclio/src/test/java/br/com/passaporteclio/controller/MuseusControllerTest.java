3813d26bc2d4cad87ef34d7222d64cc8b077ee16
Aline Napoli Fagundes <aline.martinsnapoli@hotmail.com>
2022-08-06 14:13:44 -0300

    Adição de testes de MuseusController e configurações

---
+++ passaporteclio/src/test/java/br/com/passaporteclio/controller/MuseusControllerTest.java
@@ -0,0 +1,106 @@
+package br.com.passaporteclio.controller;
+
+import br.com.passaporteclio.service.MuseusService;
+import br.com.passaporteclio.util.TokenGenerator;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.net.URI;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class MuseusControllerTest {
+
+    private TokenGenerator generator;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Mock
+    private MuseusService service;
+
+    @BeforeEach
+    public void beforeEach() {
+        MockitoAnnotations.openMocks(this);
+        generator = new TokenGenerator();
+    }
+
+    @Test
+    public void deveriaDevolver200AoCadastrarMuseu() throws Exception {
+        URI uri = new URI("/museus");
+        String json = "{\r\n"
+                + "    \"nome\": \"Museu Teste\",\r\n"
+                + "    \"descricaoMuseu\": \"Criação de um museu de testes.\",\r\n"
+                + "    \"funcionamentoMuseu\": \"De segunda a segunda, das 10h às 22h.\",\r\n"
+                + "    \"urlFoto\": \"https://www.elasviajando.com.br/wp-content/uploads/2018/09/museu-catavento-em-sao-paulo-720x550.jpg\",\r\n"
+                + "    \"urlSite\": \"museuteste.org.br\",\r\n"
+                + "    \"urlInstagram\": \"instagram.com/museuteste\",\r\n"
+                + "    \"endereco\": {\r\n"
+                + "        \"cep\": \"03033030\",\r\n"
+                + "        \"rua\": \"Av. Testes\",\r\n"
+                + "        \"numero\": \" 0\",\r\n"
+                + "        \"bairro\": \"Parque dos Testes\",\r\n"
+                + "        \"cidade\": \"São Paulo\",\r\n"
+                + "        \"estado\": \"SP\",\r\n"
+                + "        \"pais\": \"BR\"\r\n"
+                + "    }\r\n"
+                + "}";
+
+        mockMvc.
+                perform(
+                        MockMvcRequestBuilders
+                                .post(uri)
+                                .header("Authorization", "Bearer " + generator.obterToken(mockMvc))
+                                .content(json)
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers
+                        .status()
+                        .is(200));
+    }
+
+//	@Test
+//	public void testFindAll() {
+//		assertTrue(true);
+//	}
+//
+//	@Test
+//	public void testFindById() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testFindByNome() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetNotaMediaMuseu() {
+//		fail("Not yet implemented");
+//	}
+
+//	@Test
+//	public void testUpdate() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testDelete() {
+//		fail("Not yet implemented");
+//	}
+
+}
