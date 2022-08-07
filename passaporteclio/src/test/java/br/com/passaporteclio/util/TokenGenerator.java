9670021df2a45a0718f6872fdb1457d288ddb94d
Aline Napoli Fagundes <aline.martinsnapoli@hotmail.com>
2022-08-07 13:32:46 -0300

    Refatoração da classe TokenGenerator com adição de método para obter token de visitante

--- passaporteclio/src/test/java/br/com/passaporteclio/util/TokenGenerator.java
+++ passaporteclio/src/test/java/br/com/passaporteclio/util/TokenGenerator.java
@@ -5,13 +5,12 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.net.URI;
 
 public class TokenGenerator {
 
-    public String obterToken(MockMvc mockMvc) throws Exception {
+    public String obterTokenAdmin(MockMvc mockMvc) throws Exception {
         URI uri = new URI("/auth");
         String json = "{\"email\":\"admin@email.com\",\"senha\":\"123\"}";
 
@@ -21,10 +20,46 @@ public class TokenGenerator {
                                 MockMvcRequestBuilders
                                         .post(uri)
                                         .content(json)
-                                        .contentType(MediaType.APPLICATION_JSON))
-                        .andExpect(MockMvcResultMatchers
-                                .status()
-                                .is(200));
+                                        .contentType(MediaType.APPLICATION_JSON));
+
+        String resultString = result.andReturn().getResponse().getContentAsString();
+
+        JacksonJsonParser jsonParser = new JacksonJsonParser();
+        return jsonParser.parseMap(resultString).get("token").toString();
+    }
+
+    private static void cadastrarVisitante(MockMvc mockMvc) throws Exception {
+        URI uri = new URI("/visitante");
+        String json = "{\r\n"
+                + "    \"nome\": \"Visitante\",\r\n"
+                + "    \"sobrenome\": \"de Testes.\",\r\n"
+                + "    \"user\": {\r\n"
+                + "        \"email\": \"visitante@email.com\",\r\n"
+                + "        \"senha\": \"123\"\r\n"
+                + "    }\r\n"
+                + "}";
+
+        mockMvc.
+                perform(
+                        MockMvcRequestBuilders
+                                .post(uri)
+                                .content(json)
+                                .contentType(MediaType.APPLICATION_JSON));
+    }
+
+    public String obterTokenVisitante(MockMvc mockMvc) throws Exception {
+        cadastrarVisitante(mockMvc);
+
+        URI uri = new URI("/auth");
+        String json = "{\"email\":\"visitante@email.com\",\"senha\":\"123\"}";
+
+        ResultActions result =
+                mockMvc.
+                        perform(
+                                MockMvcRequestBuilders
+                                        .post(uri)
+                                        .content(json)
+                                        .contentType(MediaType.APPLICATION_JSON));
 
         String resultString = result.andReturn().getResponse().getContentAsString();
 
